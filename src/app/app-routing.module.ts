caa623328835a3d92404de4bcc12def812eafeb8
Pablo Córdoba <pablo.cordoba@bromus.com.ar>
2023-04-10 19:43:22 -0300

    Se añaden estilos básicos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AverageComponent } from './features/average/average.component';
+import { LoginComponent } from './core/login/login.component';
+import { WelcomeComponent } from './core/welcome/welcome.component';
+import { ErrorComponent } from './core/error/error.component';
 
 const routes: Routes = [
 	{ path:'average', component:AverageComponent },
+	{ path:'login', component:LoginComponent },
+	{ path:'welcome', component:WelcomeComponent },
+	{ path:'error', component:ErrorComponent },
 ];
 
 @NgModule({
